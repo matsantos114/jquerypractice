14e4229646f04479453fcaa1f63f3c300272e00d
matsantos114 <matsantos114@gmail.com>
2021-10-12 21:03:03 -0600

    jquery pratcice

---
+++ app.js
@@ -0,0 +1,9 @@
+$('.navItem').css('color', 'white')
+
+$('#nav').css('background-color', '#145')
+
+$('ul').css('background-color', '#ccc')
+
+$('.odd').css('background-color', '#aaa')
+
+$('#myInput').val('Mathew')
